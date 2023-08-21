16ea4e2b5a7d12f9f97556825653f7723f365332
juyeon-oh <83529552+OH-JUYEON@users.noreply.github.com>
2023-08-21 14:42:13 +0900

    mypage add

--- AG/urls.py
+++ AG/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from member.view import V0005, V0015, V0016
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -38,3 +40,6 @@ urlpatterns = [
     ),
 
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
