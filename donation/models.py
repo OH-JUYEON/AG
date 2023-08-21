056b44f0b40efbe01a9841439aed99422384bd77
juyeon-oh <83529552+OH-JUYEON@users.noreply.github.com>
2023-08-21 09:49:38 +0900

    table 수정

--- donation/models.py
+++ donation/models.py
@@ -5,28 +5,21 @@ from member.models import Member
 
 
 # Create your models here.
-class DonationHeader(Period, Validity):
+class Donation(Period, Validity):
     member = models.ForeignKey(Member, null=False, on_delete=models.CASCADE)
     donation_title = models.CharField(null=False, max_length=256)
     donation_minimum_amount = models.IntegerField(null=False, default=0)
     donation_status = models.SmallIntegerField(null=False, default=0)
-
-    class Meta:
-        db_table = "tbl_donation_header"
-
-
-class DonationDetail(models.Model):
-    donation_header = models.ForeignKey(DonationHeader, null=False, on_delete=models.CASCADE)
     donation_description = models.CharField(null=False, max_length=256)
     donation_content = models.CharField(null=False, max_length=10240)
-    donation_image = models.ImageField(null=False, blank=False, upload_to='DonationDetail/%Y/%m/%d')
+    donation_image = models.ImageField(null=False, blank=False, upload_to='Donation/%Y/%m/%d')
 
     class Meta:
-        db_table = "tbl_donation_detail"
+        db_table = "tbl_donation"
 
 
 class DonationInquiry(Period):
-    donation_header = models.ForeignKey(DonationHeader, null=False, on_delete=models.CASCADE)
+    donation_header = models.ForeignKey(Donation, null=False, on_delete=models.CASCADE)
     member = models.ForeignKey(Member, null=False, on_delete=models.CASCADE)
     donation_inquiry_type = models.CharField(null=False, max_length=10)
     donation_inquiry_content = models.CharField(null=False, max_length=1024)
@@ -46,7 +39,7 @@ class DonationInquiryAnswer(Period):
 
 
 class DonationReply(Period):
-    donation_header = models.ForeignKey(DonationHeader, null=False, on_delete=models.CASCADE)
+    donation_header = models.ForeignKey(Donation, null=False, on_delete=models.CASCADE)
     member = models.ForeignKey(Member, null=False, on_delete=models.CASCADE)
     donation_reply_content = models.CharField(null=False, max_length=1024)
     donation_status = models.SmallIntegerField(null=False, default=0)
@@ -56,7 +49,7 @@ class DonationReply(Period):
 
 
 class DonationDoner(models.Model):
-    donation_header = models.ForeignKey(DonationHeader, null=False, on_delete=models.CASCADE)
+    donation_header = models.ForeignKey(Donation, null=False, on_delete=models.CASCADE)
     member = models.ForeignKey(Member, null=False, on_delete=models.CASCADE)
     donate_amount = models.IntegerField(null=False, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
