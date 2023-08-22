259d3eb0f54335bfa4a10491c02ce1c3ae2a1d1a
AlainaKIM <console.log.hyebin@gmail.com>
2023-08-22 09:51:50 +0900

    donation view push test-v

--- donation/view/V0001.py
+++ donation/view/V0001.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+
+from donation.models import Donation
 
 
 # Create your views here.
 class DonationDetail(View):
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'donation/donation__001/_T001.html')
+
+        donation_id = kwargs.get('donation_id')
+
+        donation = get_object_or_404(Donation, id=donation_id)
+
+        context = {
+            'donation': donation
+        }
+
+        # datas = {
+        #     'donation_title': Donation.objects.values()[0]['donation_title'],
+        #     'donation_minimum_amount': Donation.objects.values()[0]['donation_minimum_amount'],
+        #     'donation_description': Donation.objects.values()[0]['donation_description'],
+        #     'donation_content': Donation.objects.values()[0]['donation_content'],
+        #     'donation_image': Donation.objects.values()[0]['donation_image']
+        # }
+
+        return render(request, 'funding_donation/donation__001/_T001.html', context)
