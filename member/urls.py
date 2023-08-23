e22102b0892d142e4f648b2b5fe22ca66074b0e9
juyeon-oh <83529552+OH-JUYEON@users.noreply.github.com>
2023-08-23 16:18:20 +0900

    model, mypage 추가

--- member/urls.py
+++ member/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
-from member.view import V0001, V0010, V0020, V0030, V0035, V0040, V0045, V0050, V0060, V0070, V0055
+from member.view import V0001, V0010, V0020, V0030, V0035, V0040, V0045, V0050, V0060, V0070, V0055, V0065
 
 app_name = 'member'
 
@@ -12,6 +12,7 @@ urlpatterns = [
             path("account/", V0010.AccountModify.as_view(), name="account"),
             path("email/", V0020.EmailModify.as_view(), name="email"),
             path("account/delete/", V0055.AccountDel.as_view(), name="account"),
+            path("activity/review/<int:page>/", V0065.ReviewList.as_view(), name="activityreview"),
             path("activity/<int:member_id>/<str:type>/", V0030.ActivityModify.as_view(), name="activity"),
             path("cash/", V0040.Cash.as_view(), name="cash"),
             path("cashhistory/", V0050.CashHistory.as_view(), name="cashhistory"),
--- member/view/V0050.py
+++ member/view/V0050.py
@@ -2,10 +2,50 @@ from django.shortcuts import render, redirect
 from django.views import View
 
 from member.models import Member
-
+from pay.models import Pay
 
 # Create your views here.
 class CashHistory(View):
-
+    # ag_type
+    # 0 - 충전
+    # 1 - 펀딩
+    # 2 - 기부
     def get(self, request, *args, **kwargs):
+        member_id=2
+        pay_obj = Pay.objects.filter(member_id=member_id).order_by('-id')
+        dataList = {}
+        ag_type=0
+        for index,key in enumerate(pay_obj):
+            if key.funding_id:
+                ag_type=1
+            elif key.donation_id:
+                ag_type=2 
+            data={
+                    'date':key.create_date.strftime('%Y.%m.%d'),
+                    'type':ag_type,
+                    'pay_amount':key.pay_amount,
+                    'pay_allowance':key.pay_allowance
+                }
+            
+            dataList[index] = data
+        user_pay_amount = pay_obj[0].pay_amount
+
+        return render(request, 'mypage/mypage__006/_T006.html',{'context':dataList,'user_pay_amount':user_pay_amount})
+    
+
+    def post(self, request, *args, **kwargs):
+
+        id = request.POST.get("member_id")
+        pay = int(request.POST.get("pay"))
+        pay_amount = 0
+        pay_obj = Pay.objects.filter(member_id=id).order_by('-id')
+        if pay_obj:
+           pay_amount = pay_obj[0].pay_amount
+
+        Pay.objects.create(
+            member_id=id, pay_allowance=pay, pay_amount=pay_amount+pay
+        )
+
+
+
         return render(request, 'mypage/mypage__006/_T006.html')
