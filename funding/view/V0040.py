cb2f02c580aaa63d8da52440d0c3630538a5d637
juyeon-oh <83529552+OH-JUYEON@users.noreply.github.com>
2023-08-29 05:49:50 +0900

    search add

--- funding/view/V0040.py
+++ funding/view/V0040.py
@@ -3,6 +3,7 @@ from django.views import View
 from funding.models import Funding
 from donation.models import Donation
 from campaign.models import Campaign
+from django.db.models import Count, F
 
 
 # Create your views here.
@@ -10,8 +11,7 @@ class Search(View):
 
     def get(self, request, *args, **kwargs):
         query = request.GET.get('q')
-        campaign = Funding.objects.filter(funding_content__icontains=query)
-        funding = Campaign.objects.filter(campaign_content__icontains=query)
-        donation = Donation.objects.filter(donation_content__icontains=query)
-
-        return render(request, 'funding_donation/search/_T001.html',{'campaign':campaign,'funding':funding,'donation':donation})
+        campaign = Campaign.objects.annotate(campaign_participant_count=Count('campaignparticipant__campaign_id')).filter(campaign_content__icontains=query,status=1).order_by('-id')
+        funding = Funding.objects.annotate(funding_sponsor_count=Count('fundingsponsor__funding_id')).filter(funding_content__icontains=query,status=1).order_by('-id')
+        donation = Donation.objects.annotate(donation_doner_count=Count('donationdoner__donation_id')).filter(donation_content__icontains=query,status=1).order_by('-id')
+        return render(request, 'funding_donation/search/_T001.html',{'campaign':campaign,'len1':campaign.count(), 'funding':funding,'len2':funding.count(),'donation':donation,'len3':donation.count()})
