40135ff8174b770b86b3d22a2abb778d166b980e
AlainaKIM <console.log.hyebin@gmail.com>
2023-08-31 16:48:33 +0900

    캠페인 목록 완료

--- campaign/view/V0001.py
+++ campaign/view/V0001.py
@@ -1,31 +1,64 @@
 from django.db.models import Count
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views import View
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from campaign.models import Campaign
-from region.models import CityHeader
+from campaign.view.serializers import CampaignSerializer
+from region.models import CityHeader, CityDetail
+import json
+from django.http import JsonResponse
+from django.core import serializers
 
 
 # Create your views here.
 class CampaignList(View):
-
     def get(self, request, *args, **kwargs):
+        print('들어옴')
+        return render(request, 'campaign/campaign__001/_T001.html')
 
-        campaigns = Campaign.objects.filter(campaign_category='지역순찰')\
-                        .annotate(member_count=Count('member')) \
-                        .order_by('-member_count')[:4][:4]
+
+class CampaignListAPI(APIView):
+
+    def get(self, request):
         city_headers = CityHeader.objects.all()
-
-        context = {
-            'campaigns': campaigns
-        }
-
         regions = []
+        all_campaigns = []
+        result = {}
         for city_header in city_headers:
             regions.append({
                 'city_name': city_header.city_name
             })
 
-        context['regions'] = regions
+            campaign_list = Campaign.objects.filter(id=CityHeader.objects.get(city_name=city_header.city_name).id).order_by('id')[:4]
+            campaigns = []
+            for campaign in campaign_list:
+                data = {'title': campaign.campaign_title, 'url': campaign.campaign_image.url, 'description1': campaign.campaign_description1,
+                        'description2': campaign.campaign_description2, 'description3': campaign.campaign_description3}
+                campaigns.append(data)
+            all_campaigns.append(campaigns)
 
-        return render(request, 'campaign/campaign__001/_T001.html', context)
+        result['regions'] = regions
+        result['all_campaigns'] = all_campaigns
+
+        return Response(result)
+
+    def post(self, request):
+        categories = ['지역순찰', '미화', '봉사활동', '캠페인']
+        try:
+            data = json.loads(request.body)
+            filtered_campaigns = Campaign.objects.all()
+
+            if data.get('type'):
+                filtered_campaigns = filtered_campaigns.filter(campaign_category=categories[data['type']])
+
+            if data['name'] != "지역선택":
+                location = CityDetail.objects.get(city_detail_name=data['name'])
+                filtered_campaigns = filtered_campaigns.filter(city_detail_id=location.id)
+
+        except json.JSONDecodeError as e:
+            return Response({'error': 'Invalid JSON'})
+
+        return Response(CampaignSerializer(filtered_campaigns, many=True).data)
